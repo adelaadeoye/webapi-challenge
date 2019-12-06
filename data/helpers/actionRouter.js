e88af99d6ec9ac7290ab60385a0f8eb2e1bdd2a3
adeoye adela <adeoye.adela@gmail.com>
2019-12-06 21:06:26 +0100

    delete action  working

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -3,59 +3,83 @@ const actionDB = require("./actionModel");
 const projectDB = require("./projectModel");
 const router = express.Router();
 
-
 //get all actions
-router.get('/',(req,res)=>{
-
-    actionDB.get(req.params.id)
-    .then(actions=>{
-        res.status(200).json(actions)
+router.get("/", (req, res) => {
+  actionDB
+    .get(req.params.id)
+    .then(actions => {
+      res.status(200).json(actions);
     })
-    .catch(error=>{
-        console.log(error)
-    })
-})
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 //create action
 
-router.post("/postAction",async (req,res)=>{
-    const {project_id}=req.body;
-    try{
-        const project= await projectDB.get(project_id);
-        if(!project){
-        return res.status(400).json({ message: "invalid product ID or product does not exist" });
-             }
-        else{
-            actionDB.insert(req.body)
-            .then(success=>{
-                res.status(201).json(success)
-            })
-        }
-
-
-
+router.post("/postAction", async (req, res) => {
+  const { project_id } = req.body;
+  try {
+    const project = await projectDB.get(project_id);
+    if (!project) {
+      return res
+        .status(400)
+        .json({ message: "invalid Project ID or Project does not exist" });
+    } else {
+      actionDB.insert(req.body).then(success => {
+        res.status(201).json(success);
+      });
     }
-    catch(error){
-        return res
-        .status(500)
-        .json({ error: error.message });    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
-    }
-)
-//custom Middleware
-// function validateWithPostId(req,res,next){
-//     const postID = req.body.project_id;
+//Delete action
 
-//     projectDB.get().then(users => {
-//       const result = users.find(({ id }) => id == postID);
-//       if (result) {
-//         next();
-//       } else {
-//         res.status(400).json({ message: "invalid product ID or product does not exist" });
-//       }
+// router.delete("/deleteAction/:id",  (req, res) => {
+//   actionDB
+//     .remove(req.params.id)
+//     .then(deleteSuccess => {
+//       res
+//         .status(200)
+//         .json({ message: "Item deleted successfully", deleteSuccess });
+//     })
+//     .catch(error => {
+//       res.status(500).json({ error: error.message });
 //     });
-//     next();
-// }
+// });
+router.delete("/deleteAction/:id", async (req, res) => {
+    const  actionId = req.params.id;
+    try {
+
+      const action = await actionDB.get(actionId);
+      if (!action) {
+        return res
+          .status(400)
+          .json({ message: "invalid action ID or action does not exist" });
+      } else {
+        actionDB.remove(req.params.id).then(success => {
+          res.status(200).json({message:"item deleted successfully"});
+        });
+      }
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  });
+//custom Middleware
+function validateAcionID(req, res, next) {
+  const actionId = req.params.id;
+
+  actionDB.get().then(users => {
+    const result = users.find(({ id }) => id == actionId);
+    if (result) {
+      next();
+    } else {
+      res.status(400).json({ message: "invalid  ID or action does not exist" });
+    }
+  });
+  next();
+}
 
 module.exports = router;
-
